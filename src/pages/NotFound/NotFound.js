5c891c2c2ec8bf2148e8943eae4dbcb7a3cc8b18
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-02 05:32:47 +0600

    Added Home Routing

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div>
             <div style={{height: '48vh'}} className="d-flex justify-content-center align-items-center">
-                <h2 className="text-center text-danger">404 Not Found</h2>
+                <div>
+                    <h2 className="text-center text-danger">404 Not Found</h2>
+                    <div className="text-center mt-3">
+                        <Link to="/">Back to Home?</Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
